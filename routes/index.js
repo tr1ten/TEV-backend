3c1d0436890b14fbc0657483cb5921a8c281e6ff
tr1ten <shubhi.blog@gmail.com>
2023-11-17 13:40:54 +0530

    feat(route): misc routes for fields

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
-
+const User = require('../models/User');
 /* GET home page. */
 router.get('/', function(req, res, next) {
    return res.send('Hello, World!');
+});
+
+router.get('/domains', async (req, res) => {
+  try {
+    const uniqueDomains = await User.distinct('domain');
+    res.json({ uniqueDomains });
+  } catch (error) {
+    console.error('Error retrieving unique domains:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+router.get('/genders', async (req, res) => {
+  try {
+    const uniqueGenders = await User.distinct('gender');
+    res.json({ uniqueGenders });
+  } catch (error) {
+    console.error('Error retrieving unique genders:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 module.exports = router;
